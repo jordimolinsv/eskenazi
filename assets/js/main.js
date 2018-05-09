403aefae8bc88822a150f4d6cae32d5c0586a1c0
Jordi Molins <jordimolinsv@gmail.com>
2018-05-09 08:15:01 +0200

    Slick

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,19 +6,32 @@ $(document).ready(function() {
 		e.preventDefault();
 	});
 
-	/* Slick */
+	/* Slick projects */
 	$('.proyectos').slick({
 		draggable: false,
 		fade: true,
 		mobileFirst: true,
-		nextArrow: $('.next-project'),
-		prevArrow: $('.prev-project')
+		infinite: true,
+		prevArrow: $('.prev-project'),
+		nextArrow: $('.next-project')
 	});
 
+	/* Slick slides */
+	$('.slides').slick({
+		draggable: false,
+		fade: true,
+		mobileFirst: true,
+		infinite: false,
+		prevArrow: $('.prev-slide'),
+		nextArrow: $('.next-slide')
+	});
+
+	/* Get hash and go to slide */
 	if(window.location.hash) {
 		$('.proyectos').slick('slickGoTo', window.location.hash.substring(1));
 	}
 
+	/* Update URL */
 	$('.proyectos').on('beforeChange', function(event, slick, currentSlide, nextSlide){
 		var baseUrl = window.location.href.split('#')[0];
 		window.location.replace( baseUrl + '#' + nextSlide );
