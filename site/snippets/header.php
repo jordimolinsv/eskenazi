a0251df16e8cf628025932a05cde5850e0356a36
Jordi Molins <jordimolinsv@gmail.com>
2018-11-15 20:24:32 +0100

    Robots no-index

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,6 +5,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+  <?php if($page->parent()->slug() === 'home') : ?>
+    <meta name="robots" content="noindex" />
+  <?php endif ?>
+
   <title><?= $site->title()->html() ?></title>
   <meta name="description" content="<?= $site->description()->html() ?>">
 
